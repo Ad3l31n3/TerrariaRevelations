339abf253a80c4978026fc7d4e3d8be36fa00689
44thPrince <benbdarby@gmail.com>
2025-09-06 16:55:59 -0400

    (Re)added Starwalker

--- Content/Players/RevelationsPlayer.cs
+++ Content/Players/RevelationsPlayer.cs
@@ -7,11 +7,13 @@ namespace Revelations.Content.Players
     {
         public int AccessoryDefenseBoost;
         public bool hasKirara;
+        public bool hasStarwalker;
 
         public override void ResetEffects()
         {
             AccessoryDefenseBoost = 0;
             hasKirara = false;
+            hasStarwalker = false;
         } 
         public override void PostUpdateEquips()
         {
